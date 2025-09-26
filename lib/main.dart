f473b1921b204306cbd6b2ac16ddb46312185888
msnthr0pe <pavlid66@gmail.com>
2025-09-26 21:28:10 +0300

    Spelling fixes

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child:
-        Text('ФИО - Павловсий Илья Дмитриевич\nНомер группы - ИКБО-06-2\nНомер студенческого билета - 22И0369n\n',
+        Text('ФИО - Павловский Илья Дмитриевич\nНомер группы - ИКБО-06-22\nНомер студенческого билета - 22И0369\n',
         textAlign: TextAlign.center,
         ),
       ),
