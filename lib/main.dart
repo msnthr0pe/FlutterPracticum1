c06cf97f17dee9a295078e1c644e126efd19e5f1
msnthr0pe <pavlid66@gmail.com>
2025-10-03 23:51:11 +0300

    Added bottom bar + first and second screens

--- lib/main.dart
+++ lib/main.dart
@@ -10,70 +10,66 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'My Flutter Demo Home Page'),
+      home: const MainScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = const [
+    ScreenOne(),
+    ScreenTwo(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Главная"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Поиск"),
+        ],
       ),
-      body: Center(
-        child:
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                    padding: EdgeInsets.all(100),
-                  child: Text('ФИО - Павловский Илья Дмитриевич\nНомер группы - ИКБО-06-22\nНомер студенческого билета - 22И0369\n',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.amber
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(100),
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(30)
-                  ),
-                  child: ElevatedButton(
-                    onPressed: null,
-                    style: ButtonStyle(
-                        backgroundColor: WidgetStateProperty.all(Colors.amber),
-                        shape:
-                        WidgetStateProperty.all(
-                            RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30)
-                            )
-                        )
-                    ),
-                    child: Text("Тестовая кнопка"),
-                  ),
-                )
-              ],
-            )
-      ),
+    );
+  }
+}
+
+class ScreenOne extends StatelessWidget {
+  const ScreenOne({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Экран 1: Главная", style: TextStyle(fontSize: 24)),
+    );
+  }
+}
+
+class ScreenTwo extends StatelessWidget {
+  const ScreenTwo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Экран 2: Поиск", style: TextStyle(fontSize: 24)),
     );
   }
 }
